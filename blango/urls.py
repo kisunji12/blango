675ce404593791332526efffc09620f718651b16
Krishna Phuyal <krisnaphuyal@gmail.com>
2022-09-20 03:11:11 +0000

    Finish django allauth

--- blango/urls.py
+++ blango/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path("accounts/register/",RegistrationView.as_view(form_class=BlangoRegistrationForm),name="django_registration_register",),
     path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"), 
     path("ip/", blog.views.get_ip),
     path("", blog.views.index),
